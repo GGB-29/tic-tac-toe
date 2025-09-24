40b2a398227ce8de9c70317d9d882ebc9dd2487b
GGB-29 <216197362+GGB-29@users.noreply.github.com>
2025-09-24 12:19:29 +0100

    feat: Add game logic to play game.
    Excluding check win logic.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 function createPlayer (name) {
     let playerScore = 0;
     const playerName = name;
+    const playerSymbol = symbol;
     
     const getScore = () => (score);
     const setScore = () => score++;
@@ -13,15 +14,55 @@ function createPlayer (name) {
 
 //gameboard factory function
 function createGameboard () {
-    let gameboard = [ '', '', '', 
-                      '', '', '',
-                      '', '', ''
+    let gameboard = [ 
+                      ['', '', ''], 
+                      ['', '', ''], 
+                      ['', '', '']
                     ];
-    const addCounter = (player, index) => {gameboard[index] = player;};
+    const addCounter = (player, xpos, ypos) => {gameboard[ypos, xpos] = player;};
     const getGameboard = () => (gameboard);
+    const reset = () => gameboard = [ 
+                                      ['', '', ''], 
+                                      ['', '', ''], 
+                                      ['', '', '']
+                                    ];
     return {addCounter, getGameboard};
 };
 
+//create game module
 const gameObject = (function () {
+    const gameboardObject = createGameboard();
 
+    const playerOne = createPlayer('test1');
+    const playerTwo = createPlayer('test2');
+    let firstPlayer = playerOne;
+    let currentPlayer = firstPlayer;
+    let turnNumber = 1; //odd turns -> cross, even -> circle
+
+    //checks if there is a winner
+    const isGameWon = () => {
+
+    };
+
+    //called each time a box is clicked
+    const playTurn = (xpos, ypos) => {
+        gameboardObject.addCounter(currentPlayer, xpos, ypos);
+        isGameWon();
+        turnNumber++;
+        currentPlayer = (currentPlayer === playerOne) ? playerTwo : playerOne;
+    }
+
+    const playGame = () => {
+        while (!isGameWon)  {
+            continue;
+        }
+        let winner = currentPlayer;
+        currentPlayer.setScore;
+        gameboardObject.reset();
+        
+        //change player who goes first
+        firstPlayer = (firstPlayer === playerOne) ? playerTwo : playerOne;
+    }
 })();
+
+gameObject.playGame()
