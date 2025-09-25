9d85c7640d264f94da2ba57738fe0302502d375d
GGB-29 <216197362+GGB-29@users.noreply.github.com>
2025-09-25 16:06:33 +0100

    feat: Add functions to check game win.

--- script.js
+++ script.js
@@ -40,29 +40,46 @@ const gameObject = (function () {
     let turnNumber = 1; //odd turns -> cross, even -> circle
 
     //checks if there is a winner
-    const isGameWon = () => {
+    const isGameWon = () => (isRowWon() || isColumnWon() || isDiagonalWon());
 
+    const isRowWon = () => {
+        for (let y = 0; y < 3; y++) {
+            if (gameboardObject[y][0] === gameboardObject[y][1] && gameboardObject[y][1] === gameboardObject[y][2]) {
+                return true;
+            }
+        }
+        return false;
+    };
+    const isColumnWon = () => {
+        for (let x = 0; x < 3; x++) {
+            if (gameboardObject[0][x] === gameboardObject[1][x] && gameboardObject[1][x] === gameboardObject[2][x]) {
+                return true;
+            }
+        }
+        return false;
+    };
+    const isDiagonalWon = () => {
+        return ((gameboardObject[0][0] === gameboardObject[1][1] && gameboardObject[1][1] === gameboardObject[2][2]) 
+            || (gameboardObject[0][2] === gameboardObject[1][1] && gameboardObject[1][1] === gameboardObject[2][0]));
     };
 
     //called each time a box is clicked
     const playTurn = (xpos, ypos) => {
         gameboardObject.addCounter(currentPlayer, xpos, ypos);
-        isGameWon();
         turnNumber++;
         currentPlayer = (currentPlayer === playerOne) ? playerTwo : playerOne;
-    }
-
-    const playGame = () => {
-        while (!isGameWon)  {
-            continue;
+        if (isGameWon()) {
+            endGame();
         }
-        let winner = currentPlayer;
+    };
+
+    const endGame = () => {
+        //winner is player of previous turn
+        let winner = (currentPlayer === playerOne) ? playerTwo : playerOne;
         currentPlayer.setScore;
         gameboardObject.reset();
         
         //change player who goes first
         firstPlayer = (firstPlayer === playerOne) ? playerTwo : playerOne;
-    }
+    };
 })();
-
-gameObject.playGame()
